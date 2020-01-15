1155a0bd52e940e68bbb3b66dba272df547164f6
Jamil Zazu <jamilzazu@gmail.com>
2020-01-14 23:41:12 -0400

    Estilização da Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,105 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { MdShoppingCart } from 'react-icons/md';
+import { ProductList } from './styles';
 
 export default function Home() {
   return (
-    <div />
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/bnn/l_netshoes/2019-12-06/2592_Trio3.jpg"
+          alt="Tenis"
+        />
+        <strong>Tenis teste</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/bnn/l_netshoes/2019-12-06/2592_Trio3.jpg"
+          alt="Tenis"
+        />
+        <strong>Tenis teste</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/bnn/l_netshoes/2019-12-06/2592_Trio3.jpg"
+          alt="Tenis"
+        />
+        <strong>Tenis teste</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/bnn/l_netshoes/2019-12-06/2592_Trio3.jpg"
+          alt="Tenis"
+        />
+        <strong>Tenis teste</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/bnn/l_netshoes/2019-12-06/2592_Trio3.jpg"
+          alt="Tenis"
+        />
+        <strong>Tenis teste</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/bnn/l_netshoes/2019-12-06/2592_Trio3.jpg"
+          alt="Tenis"
+        />
+        <strong>Tenis teste</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
   );
 }
